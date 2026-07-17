09b35044ffc2b85e120739ce51e336a0704cc642
Andre Stubbe <andrestubbe@proton.me>
2026-07-17 08:01:27 +0200

    Auto Update

--- src/main/java/fasttui/component/DividerVertical.java
+++ src/main/java/fasttui/component/DividerVertical.java
@@ -1,7 +1,24 @@
 package fasttui.component;
 
-public class DividerVertical extends Control{
+import fastterminal.FastTerminalScene;
+
+public class DividerVertical extends Control {
+
+    private int color = 0x555555;
+
     public DividerVertical(int x, int y, int width, int height) {
         super(x, y, width, height);
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    @Override
+    public void render(FastTerminalScene scene) {
+        if (!visible) return;
+        for (int r = 0; r < height; r++) {
+            scene.writeCell(x, y + r, '│', color, backgroundColor);
+        }
     }
 }
